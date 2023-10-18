ae661ae67cefaa0def2a59e967926616206df17f
dineshG0000 <148310259+dineshG0000@users.noreply.github.com>
2023-10-18 16:30:35 +0530

    Add files via upload

---
+++ batsman.py
@@ -0,0 +1,17 @@
+class Player:
+    def play(self):
+        pass
+
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
+
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+
+batsman = Batsman()
+bowler = Bowler()
+
+batsman.play()
+bowler.play()
